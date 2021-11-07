8021debf66bf600969971775c00566a8fab2adfc
loganisme <ngoanhtue312005@gmail.com>
2021-11-07 10:21:53 +0700

    $ git add ,

---
+++ Session2_Github_landingpage/assets/app.js
@@ -0,0 +1,8 @@
+function alert_userType(){
+    var a = document.forms['form']['name'].value;
+    var b = document.forms['form']['email'].value;
+    var c = document.forms['form']['quiz'].value;
+    var d = document.forms['form']['newsletter'].value;
+    var e = document.forms['form']['message'].value;
+    alert(a);
+}
