b0985520f52b43c6096c390eaace47ae48ab579f
loganisme <ngoanhtue312005@gmail.com>
2021-11-13 15:55:48 +0700

    update

--- Session2_Github_landingpage/assets/app.js
+++ Session2_Github_landingpage/assets/app.js
@@ -1,18 +1,17 @@
-function validate(){
-    let name = document.forms['form']['name'].value;
-    let email = document.forms['form']['email'].value;
-    let mess = document.forms['form']['message'].value;
-    // alert('tên bạn là :' + name + '\n' + 'email bạn là:' + email)
-    if(email.includes('@gmail.com')){
-        document.writeln('Chào '+name+'<br>')
-        document.writeln('Email của bạn là ' + email+'<br>')
-        if(mess == ""){
-            document.writeln('bạn không muốn nói gì sao?')
-        }
-        else{document.writeln('Your message : ' + mess)}   
-        
-}
-    else{alert('vui lòng nhập đúng email')}
-
-
+function validate() {
+  let name = document.forms["form"]["name"].value;
+  let email = document.forms["form"]["email"].value;
+  let mess = document.forms["form"]["message"].value;
+  // alert('tên bạn là :' + name + '\n' + 'email bạn là:' + email)
+  if (email.includes("@gmail.com")) {
+    document.writeln("Chào " + name + "<br>");
+    document.writeln("Email của bạn là " + email + "<br>");
+    if (mess == "") {
+      document.writeln("bạn không muốn nói gì sao?");
+    } else {
+      document.writeln("Your message : " + mess);
+    }
+  } else {
+    alert("vui lòng nhập đúng email");
+  }
 }
